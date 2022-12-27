6af3b6f6ad5dca4cb670910b44cdfbdd19f9939c
Basanya <Albajd@gmail.com>
2022-12-27 20:58:48 +0100

    my_api

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import decorators 
+
+# Create your views here.
+def first_api_view(request):
+    return Response
+    
